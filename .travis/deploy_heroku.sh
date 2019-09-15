37cee6ef7541915f9dc0fc072587af59ebae6940
jelmerdejeger@gmail.com <jelmerdejeger@gmail.com>
2019-09-15 21:18:27 +0200

    traviscit est

--- .travis/deploy_heroku.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-docker login -u $DOCKER_USER --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
-heroku container:release web --app $HEROKU_APP_NAME
